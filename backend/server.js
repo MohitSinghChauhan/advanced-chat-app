d253a209321280666db3c3bf983c270402c9f333
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-04-07 14:41:12 +0530

    Chats API Endpoint done

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const { chats } = require('./data/data.js');
 
-const app  = express();
+const app = express();
 
-app.get('/',(req,res) => {
-    res.send("API is Running");
-})
+app.get('/', (req, res) => {
+  res.send('API is Running');
+});
 
-app.get('/api/chat',(req,res) => {
+app.get('/api/chat', (req,res)=>{
     res.send(chats);
 })
 
-app.listen(5000, console.log("APP IS RUNNING ON 5000"));
+app.get('/api/chat/:id', (req, res) => {
+    //   console.log(req.params.id);
+    const singleChat = chats.find((chat)=>(
+        chat._id===req.params.id
+        ));
+        res.send(singleChat);
+    });
+
+app.listen(5000, console.log('APP IS RUNNING ON 5000'));
