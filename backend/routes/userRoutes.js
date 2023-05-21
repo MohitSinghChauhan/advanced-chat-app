771aa7a28585ce15bf4f976f0f7e5dca1e001777
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-05-21 11:21:45 +0530

    Feature: Implement API endpoint to fetch all users from MongoDB with protected middleware using JWT token verification

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const {registerUser, authUser} = require('../controllers/userController');
+const {registerUser, authUser, allUsers} = require('../controllers/userController');
+const {protect} = require('../middlewares/authMiddleware');
 
 //Chaining method, we can chain more methods in it
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, allUsers);
 
 //normal, we can,t chain here
 router.post('/login',authUser);
