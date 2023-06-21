3618261f57c12ef8b85205d1b3f8af74ca2593c0
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-06-22 04:44:45 +0530

    refactor: Homepage UI improvements

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -8,7 +8,6 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
-  Text,
 } from '@chakra-ui/react';
 import Login from '../components/Authentication/Login';
 import Signup from '../components/Authentication/Signup';
@@ -17,24 +16,25 @@ const Homepage = () => {
   return (
     <Container
       bgColor="white"
-      mt={'100px'}
+      mt={'-15px'}
       px={'30px'}
       py="10px"
       borderRadius="30px"
     >
       <Center>
-        <Heading color="black" mt="30px">
-          Devil Messages
+        <Heading color="black" mt="20px">
+          Join CodeChat
         </Heading>
       </Center>
-      <Center mt="4px">
-        <Text color="black">Chat to the Devil, Be ready to get Punish.</Text>
-      </Center>
 
-      <Tabs variant="soft-rounded" colorScheme="red" mt="40px">
+      <Tabs variant="soft-rounded" colorScheme="red" mt="20px">
         <TabList>
-          <Tab width="50%"  _hover={{bg:'red', color:'white'}} >Login</Tab>
-          <Tab width="50%"  _hover={{bg:'red', color:'white'}}>Sign Up</Tab>
+          <Tab width="50%" _hover={{ bg: 'red', color: 'white' }}>
+            Login
+          </Tab>
+          <Tab width="50%" _hover={{ bg: 'red', color: 'white' }}>
+            Sign Up
+          </Tab>
         </TabList>
         <TabPanels>
           <TabPanel>
