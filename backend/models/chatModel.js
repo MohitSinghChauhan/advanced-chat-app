713a6f738c340bb41d16c58b86166b6d32d0a042
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-06-22 00:42:54 +0530

    bug: Fixed Typo in Chat Model

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -11,7 +11,7 @@ const chatModel = mongoose.Schema(
             }
         ],
         latestMessage:{
-            type:mongoose.Schema.Types.ObjrctId,
+            type:mongoose.Schema.Types.ObjectId,
             ref:"Message",
         },
         groupAdmin:{
